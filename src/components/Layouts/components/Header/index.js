fa3c96d2163aa17d56243691fd81ab17654e01e5
VStorm <tranvubao2004@gmail.com>
2025-07-07 17:27:48 +0700

    add logo

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,12 +1,18 @@
 import classNames from 'classnames/bind';
+
 import styles from './Header.module.scss';
+import { logo } from '~/assets/images/index';
 
 const sx = classNames.bind(styles);
 
 function Header() {
     return (
         <header className={sx('wrapper')}>
-            <div className={sx('inner')}></div>
+            <div className={sx('inner')}>
+                <div className={sx('logo')}>
+                    <img src={logo} alt="Tiktok-Logo" />
+                </div>
+            </div>
         </header>
     );
 }
