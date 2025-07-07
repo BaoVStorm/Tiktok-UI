04895443bde4131c575be8037d98bc211d06b3d7
VStorm <tranvubao2004@gmail.com>
2025-07-07 20:47:01 +0700

    add logo, search

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,17 +1,36 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// icon fontawesome
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './Header.module.scss';
 import { logo } from '~/assets/images/index';
 
-const sx = classNames.bind(styles);
+const cx = classNames.bind(styles);
 
 function Header() {
     return (
-        <header className={sx('wrapper')}>
-            <div className={sx('inner')}>
-                <div className={sx('logo')}>
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
                     <img src={logo} alt="Tiktok-Logo" />
                 </div>
+
+                <div className={cx('search')}>
+                    <input placeholder="Search account and videos" spellCheck="false" />
+
+                    <button className={cx('close')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+
+                <div className={cx('actions')}></div>
             </div>
         </header>
     );
